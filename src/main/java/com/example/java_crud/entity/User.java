7e920a98ae12464c3768f2b061b2803237a80f27
Abdul Wazed Sobuz <abdulwazedsobuz@gmail.com>
2024-04-30 23:55:32 +0600

    feat: add user entity

---
+++ src/main/java/com/example/java_crud/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.java_crud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String email;
+}
